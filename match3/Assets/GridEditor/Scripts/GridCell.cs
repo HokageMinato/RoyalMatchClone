c4d6b46c6234e8ffd6504d7affd716f88e645ad0
Minato Namikaze <dsparda996@gmail.com>
2021-08-13 00:11:38 +0530

    swipe control fundamentals

--- match3/Assets/GridEditor/Scripts/GridCell.cs
+++ match3/Assets/GridEditor/Scripts/GridCell.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class GridCell : MonoBehaviour
+public class GridCell : MonoBehaviour,IPointerDownHandler,IPointerEnterHandler
 {
     public GridCell elementSource;
-    
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SwipeInputReciever.Instance.selectedCell = this;
+        SwipeInputReciever.Instance.secondCell = null;
+        Debug.Log("Clicking");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(SwipeInputReciever.Instance.selectedCell != this)
+            SwipeInputReciever.Instance.secondCell = this;
+        
+        //TODO Create a method in GridCell to check weather the second cell is neighbourCell or not, if so assign else discard.
+    }
 }
---
+++ match3/Assets/GridEditor/Scripts/SwipeInputReciever.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeInputReciever : MonoBehaviour
+{
+
+    public static SwipeInputReciever Instance;
+    
+    public void Awake()
+    {
+       Instance = this;
+    }
+
+    public GridCell selectedCell;
+    public GridCell secondCell;
+
+}
